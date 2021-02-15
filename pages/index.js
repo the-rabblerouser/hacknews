f93ba3ed6ff9dd194179db0d6834d69fa354c451
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-02-15 00:09:53 -0600

    created a data fetcher to grab stories and added each story to a list item. Some styling was included

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,66 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
-import Navbar from '../components/Navbar';
+import {
+	Container,
+	ListGroup,
+	ListGroupItem,
+	ListGroupItemHeading,
+	ListGroupItemText,
+} from 'reactstrap';
 
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+import Navbar from '../components/Navbar';
+import useDataFetcher from '../components/hooks/useDataFetcher';
+import timeAgo from '../utils/timeAgo';
+
+const Home = () => {
+	const type = 'top';
+	const { isLoading, stories } = useDataFetcher(type);
+
+	console.log(stories);
+
 	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Hacker News</title>
-				<link rel="icon" href="/y18.png" />
-			</Head>
-			<main className={styles.main}>
-				<Navbar />
-			</main>
-		</div>
+		<>
+			{isLoading ? (
+				<p>Loading...</p>
+			) : (
+				<div className={styles.container}>
+					<Head>
+						<title>Hacker News</title>
+						<link rel="icon" href="/y18.png" />
+					</Head>
+					<main className={styles.main}>
+						<Navbar />
+						<Container>
+							<ListGroup className="mt-2">
+								{stories.map(
+									({
+										data: { id, by, title, url, score, time, descendants },
+									}) => {
+										return (
+											<>
+												<ListGroupItem key={id} className="ps-5">
+													<ListGroupItemHeading>
+														<a href={url}>{title}</a>
+													</ListGroupItemHeading>
+													<ListGroupItemText className="text-muted">
+														{score} points by {by} {timeAgo(time)} |{' '}
+														{descendants} comments
+													</ListGroupItemText>
+												</ListGroupItem>
+											</>
+										);
+									}
+								)}
+							</ListGroup>
+						</Container>
+					</main>
+				</div>
+			)}
+		</>
 	);
-}
+};
+
+export default Home;
