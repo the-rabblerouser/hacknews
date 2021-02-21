8fa24e99d01ccc7bad71cf6658999e81f7ac25f4
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-02-21 13:58:25 -0600

    added background color to list items

--- components/List.js
+++ components/List.js
@@ -33,21 +33,27 @@ const List = ({ type }) => {
 					.map(({ data: { by, title, url, score, time, descendants, id } }) => {
 						return (
 							<div key={uuidv4()}>
-								<ListGroupItem className="ps-5">
+								<ListGroupItem
+									className="ps-5"
+									style={{ backgroundColor: '#F6F6EF' }}>
 									<ListGroupItemHeading>
-										<a href={url}>{title}</a>
+										<a href={url} style={{ color: 'inherit' }}>
+											{title}
+										</a>
 									</ListGroupItemHeading>
 									<ListGroupItemText className="text-muted">
 										{score} points by{' '}
 										<span>
 											<Link href={{ pathname: '/user', query: { name: by } }}>
-												<a>{by}</a>
+												<a style={{ color: 'inherit' }}>{by}</a>
 											</Link>
 										</span>{' '}
 										{timeAgo(time)} |{' '}
 										<span>
 											<Link href={`/${type}/[$id]`} as={`/${type}/${id}`}>
-												<a>{descendants} comments</a>
+												<a style={{ color: 'inherit' }}>
+													{descendants} comments
+												</a>
 											</Link>
 										</span>
 									</ListGroupItemText>
