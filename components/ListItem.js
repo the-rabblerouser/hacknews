3409ede6e1a6ba3ab5e110c6523165164642b344
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-04-18 23:37:04 -0500

    data is now fetched using swr directly in index.js page and ListItem component to display list of stories

---
+++ components/ListItem.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+import useSWR from 'swr';
+
+import {
+	ListGroup,
+	ListGroupItem,
+	ListGroupItemHeading,
+	ListGroupItemText,
+	Spinner,
+} from 'reactstrap';
+import { v4 as uuidv4 } from 'uuid';
+
+import timeAgo from '../utils/timeAgo';
+
+import styles from '../styles/list.module.css';
+
+const ListItem = ({ item }) => {
+	const fetcher = (url) => fetch(url).then((res) => res.json());
+
+	const { data, error } = useSWR(
+		`https://hacker-news.firebaseio.com/v0/item/${item}.json?print=pretty`,
+		fetcher
+	);
+
+	if (error) return <div>failed to load</div>;
+	if (!data) return <div>loading...</div>;
+
+	const { by, title, url, score, time, descendants } = data;
+	return (
+		<>
+			<ListGroup>
+				<div key={uuidv4()}>
+					<ListGroupItem
+						className="ps-5"
+						style={{ backgroundColor: '#F6F6EF' }}>
+						<ListGroupItemHeading>
+							<a href={url} style={{ color: 'inherit' }}>
+								{title}
+							</a>
+						</ListGroupItemHeading>
+						<ListGroupItemText className="text-muted">
+							{score} points by{' '}
+							<span>
+								<Link href={{ pathname: '/user', query: { name: by } }}>
+									<a style={{ color: 'inherit' }}>{by}</a>
+								</Link>
+							</span>{' '}
+							{timeAgo(time)} |{' '}
+							<span>
+								<Link href="#">
+									<a style={{ color: 'inherit' }}>{descendants} comments</a>
+								</Link>
+							</span>
+						</ListGroupItemText>
+					</ListGroupItem>
+				</div>
+			</ListGroup>
+		</>
+	);
+};
+
+export default ListItem;
--- utils/apis.js
+++ utils/apis.js
@@ -1,25 +1,25 @@
 import axios from 'axios';
-import { BASE_API_URL } from './constants';
+// import { BASE_API_URL } from './constants';
 
-const getStory = async (id) => {
-	try {
-		const story = await axios.get(
-			`${BASE_API_URL}/item/${id}.json?print=pretty`
-		);
-		return story;
-	} catch (error) {
-		console.log('Error');
-	}
-};
+// const getStory = async (id) => {
+// 	try {
+// 		const story = await axios.get(
+// 			`${BASE_API_URL}/item/${id}.json?print=pretty`
+// 		);
+// 		return story;
+// 	} catch (error) {
+// 		console.log('Error');
+// 	}
+// };
 
-export const getStories = async (type) => {
-	try {
-		const { data: storyIds } = await axios.get(
-			`${BASE_API_URL}/${type}stories.json?print=pretty`
-		);
-		const stories = await Promise.all(storyIds.slice(0, 30).map(getStory));
-		return stories;
-	} catch (error) {
-		console.log('Error');
-	}
-};
+// export const getStories = async (type) => {
+// 	try {
+// 		const { data: storyIds } = await axios.get(
+// 			`${BASE_API_URL}/${type}stories.json?print=pretty`
+// 		);
+// 		const stories = await Promise.all(storyIds.slice(0, 30).map(getStory));
+// 		return stories;
+// 	} catch (error) {
+// 		console.log('Error');
+// 	}
+// };
