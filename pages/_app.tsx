5ca67a4b32d9e07ada3fce959957812fd9c1c905
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-07-11 08:29:19 -0500

    tsconfig and ts dev dependencies added. _app.js -> _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
 import '../styles/globals.css';
 
 import Navbar from '../components/Navbar';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
 			<Head>
 				<title>Hacker News</title>
-				<link rel="icon" href="/y18.png" />
+				<link rel='icon' href='/y18.png' />
 			</Head>
 			<Navbar />
 			<Component {...pageProps} />
